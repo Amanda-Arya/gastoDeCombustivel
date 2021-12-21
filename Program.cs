cbbc66670858d695a086d788a96c212fa9636faf
AMANDA <acv-2010@hotmail.com>
2021-12-21 17:33:40 -0300

    meu commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace gastoDeCombustivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int tempoDeViagem, velocidadeMedia;
+            double combustivel, distancia;
+
+
+            Console.WriteLine("Qual a duração da viagem em horas?");
+            tempoDeViagem = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{tempoDeViagem} horas");
+
+            Console.WriteLine("Qual a velocidade média?");
+            velocidadeMedia = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{velocidadeMedia} Km/h");
+
+            distancia = velocidadeMedia * tempoDeViagem;
+            combustivel = distancia / 12; // considerando que o carro faz 12 km/ litro
+
+            Console.WriteLine($" São necessários {Math.Round(combustivel, 3)} litros de combustível!");
+
+
+
+
+        }
+    }
+}
